9fab6a7d223f4f6893308c017c08ad23c6b9b8c5
Pedro <pvcastro@gmail.com>
2022-02-23 06:13:58 -0300

    Adding results and labels for version 5 of labeling process

--- hate_speech/model_training/train_bert_cv.py
+++ hate_speech/model_training/train_bert_cv.py
@@ -1,4 +1,4 @@
-from simpletransformers.classification import ClassificationModel, MultiLabelClassificationModel
+from simpletransformers.classification import ClassificationModel
 import argparse
 import logging
 import pandas as pd
@@ -6,8 +6,9 @@ import os
 import ast
 import json
 import numpy as np
+from pathlib import Path
 from sklearn import metrics
-from sklearn.preprocessing import LabelBinarizer, MultiLabelBinarizer
+from sklearn.preprocessing import LabelBinarizer
 from scipy.special import softmax
 from scipy import interp
 from typing import List
@@ -32,7 +33,8 @@ def get_args_from_command_line():
     parser.add_argument("--learning_rate", type=float, default=4e-5)
     parser.add_argument("--warmup_steps", type=int, default=0)
     parser.add_argument("--seed", type=int, default=42)
-    parser.add_argument("--multi_label", type=bool, default=False)
+    parser.add_argument("--folds", type=int, default=10)
+    parser.add_argument("--version", type=int, default=5)
     args = parser.parse_args()
     return args
 
@@ -75,32 +77,6 @@ def prepare_filepath_for_storing_best_model(path_to_store_model: str) -> str:
 def read_json(filename: str):
     with open(filename) as f_in:
         return json.load(f_in)
-
-
-def multilabel_class_report(y_true, y_pred, y_score):
-    if y_true.shape != y_pred.shape:
-        print("Error! y_true %s is not the same shape as y_pred %s" % (
-            y_true.shape,
-            y_pred.shape)
-              )
-        return
-
-    mlb = MultiLabelBinarizer()
-
-    actual = mlb.fit_transform(y_true)
-    pred = mlb.fit_transform(y_pred)
-
-    # Value counts of predictions
-    labels, cnt = np.unique(
-        y_true,
-        return_counts=True)
-    n_classes = len(labels)
-    pred_cnt = pd.Series(cnt, index=labels)
-
-    metrics_summary = metrics.precision_recall_fscore_support(
-        y_true=y_true,
-        y_pred=y_pred,
-        labels=labels)
 
 
 def class_report(y_true, y_pred, y_score=None, average='macro'):
@@ -200,96 +176,79 @@ if __name__ == '__main__':
     args = get_args_from_command_line()
     logger.info(f'Args: {args}')
     # load data
-    df = pd.read_csv(args.data_path)
-    if args.multi_label:
-        df['labels'] = df.apply(lambda row: ast.literal_eval(row['labels']), axis=1)
-        num_labels = len(df.iloc[0]['labels'])
-    else:
-        num_labels = len(df['labels'].unique())
-    train_df = df.sample(frac=0.7, random_state=0)
-    eval_df = df.drop(train_df.index).reset_index(drop=True)
-    train_df = train_df.reset_index(drop=True)
-    # define paths and args
-    output_dir = f"{args.output_dir}/{args.model_name.replace('/', '-')}"
-    path_to_store_model = prepare_filepath_for_storing_model(output_dir=output_dir)
-    path_to_store_best_model = prepare_filepath_for_storing_best_model(path_to_store_model)
-    SLURM_JOB_ID = get_env_var('SLURM_JOB_ID', 1)
-    classification_args = {
-        'train_batch_size': args.batch_size,
-        # 'gradient_accumulation_steps': 4,
-        'overwrite_output_dir': True,
-        'evaluate_during_training': True,
-        'save_model_every_epoch': False,
-        'save_eval_checkpoints': False,
-        'output_dir': path_to_store_model,
-        'best_model_dir': path_to_store_best_model,
-        'evaluate_during_training_verbose': True,
-        'num_train_epochs': args.num_train_epochs,
-        "learning_rate": args.learning_rate,
-        "warmup_steps": args.warmup_steps,
-        # "max_seq_length": 512,
-        # "warmup_ratio": 0.2,
-        # "scheduler": "constant_schedule",
-        "do_lower_case": False,
-        "use_early_stopping": True,
-        "early_stopping_delta": 0,
-        "early_stopping_metric": "eval_loss",
-        "early_stopping_metric_minimize": True,
-        "early_stopping_patience": 5,
-        "tensorboard_dir": f"runs/{SLURM_JOB_ID}/",
-        "manual_seed": args.seed}
-    if args.multi_label:
-        classification_args['config'] = {
-            'problem_type': 'multi_label_classification'
-        }
-        classification_args["reprocess_input_data"] = True
-        model = MultiLabelClassificationModel(args.model_type, args.model_name, num_labels=num_labels,
-                                              use_cuda=True, args=classification_args)
-    else:
+    folds = args.folds
+    version = args.version
+    folds_path = Path(args.data_path)
+    assert folds_path.is_dir()
+    for fold in range(folds):
+        train_df = pd.read_csv(folds_path / f'train_v{version}_fold-{fold}.csv')
+        eval_df = pd.read_csv(folds_path / f'test_v{version}_fold-{fold}.csv')
+        num_labels = len(train_df['labels'].unique())
+        # define paths and args
+        output_dir = f"{args.output_dir}/{args.model_name.replace('/', '-')}/fold-{fold}"
+        path_to_store_model = prepare_filepath_for_storing_model(output_dir=output_dir)
+        path_to_store_best_model = prepare_filepath_for_storing_best_model(path_to_store_model)
+        SLURM_JOB_ID = get_env_var('SLURM_JOB_ID', 1)
+        classification_args = {
+            'train_batch_size': args.batch_size,
+            'gradient_accumulation_steps': 4,
+            'overwrite_output_dir': True,
+            'evaluate_during_training': True,
+            'save_model_every_epoch': False,
+            'save_eval_checkpoints': False,
+            'output_dir': path_to_store_model,
+            'best_model_dir': path_to_store_best_model,
+            'evaluate_during_training_verbose': True,
+            'num_train_epochs': args.num_train_epochs,
+            "learning_rate": args.learning_rate,
+            "warmup_steps": args.warmup_steps,
+            # "max_seq_length": 512,
+            # "warmup_ratio": 0.2,
+            # "scheduler": "constant_schedule",
+            "do_lower_case": False,
+            "use_early_stopping": True,
+            "early_stopping_delta": 0,
+            "early_stopping_metric": "eval_loss",
+            "early_stopping_metric_minimize": True,
+            "early_stopping_patience": 5,
+            "tensorboard_dir": f"runs/{SLURM_JOB_ID}/",
+            "manual_seed": args.seed}
         model = ClassificationModel(args.model_type, args.model_name, num_labels=num_labels, use_cuda=True,
                                     args=classification_args)
-    # Train the model
-    model.train_model(train_df, eval_df=eval_df, output_dir=path_to_store_model, multi_label=args.multi_label)
-    logging.info("The training of the model is done")
-    # Load best model (in terms of evaluation loss)
-    train_args = read_json(filename=os.path.join(path_to_store_best_model, 'model_args.json'))
-    if args.multi_label:
-        best_model = MultiLabelClassificationModel(args.model_type, path_to_store_best_model, args=train_args)
-    else:
+        # Train the model
+        model.train_model(train_df, eval_df=eval_df, output_dir=path_to_store_model)
+        logging.info("The training of the model is done")
+        # Load best model (in terms of evaluation loss)
+        train_args = read_json(filename=os.path.join(path_to_store_best_model, 'model_args.json'))
         best_model = ClassificationModel(args.model_type, path_to_store_best_model, args=train_args)
-    logging.info("Loaded best model")
-    # EVALUATION ON EVALUATION SET
-    result, model_outputs, wrong_predictions = best_model.eval_model(eval_df)
-    logging.info('Get output from best model')
-    scores = np.array([softmax(element) for element in model_outputs])
-    pred = np.argmax(scores, axis=1)
-    if args.multi_label:
-        result_report_macro_df = multilabel_class_report(y_true=eval_df['labels'].to_list(),
-                                                         y_pred=pred, y_score=model_outputs)
-    else:
+        logging.info("Loaded best model")
+        # EVALUATION ON EVALUATION SET
+        result, model_outputs, wrong_predictions = best_model.eval_model(eval_df)
+        logging.info('Get output from best model')
+        scores = np.array([softmax(element) for element in model_outputs])
+        pred = np.argmax(scores, axis=1)
         result_report_macro_df = class_report(y_true=np.concatenate(eval_df[['labels']].to_numpy(), axis=0),
                                               y_pred=pred, y_score=scores, average='macro')
-    logging.info('Built evaluation report')
-    # Centralize evaluation results in a dictionary
-    slurm_job_timestamp = args.timestamp
-    slurm_job_id = SLURM_JOB_ID
+        logging.info('Built evaluation report')
+        # Centralize evaluation results in a dictionary
+        slurm_job_timestamp = args.timestamp
+        slurm_job_id = SLURM_JOB_ID
 
-    # Save evaluation results on eval set
-    if "/" in args.model_name:
-        args.model_name = args.model_name.replace('/', '-')
-    path_to_store_eval_results = os.path.join(f'{output_dir}_{str(slurm_job_id)}',
-                                              f'evaluation.csv')
-    if not os.path.exists(os.path.dirname(path_to_store_eval_results)):
-        os.makedirs(os.path.dirname(path_to_store_eval_results))
-    result_report_macro_df.to_csv(
-        path_to_store_eval_results)
-    logging.info(
-        "The evaluation on the evaluation set is done. The results were saved at {}".format(path_to_store_eval_results))
-    # Save scores
-    eval_df['score'] = scores.tolist()
-    path_to_store_eval_scores = os.path.join(f'{output_dir}_{str(slurm_job_id)}',
-                                             f'scores.csv')
-    if not os.path.exists(os.path.dirname(path_to_store_eval_scores)):
-        os.makedirs(os.path.dirname(path_to_store_eval_scores))
-    eval_df.to_csv(path_to_store_eval_scores, index=False)
-    logging.info("The scores for the evaluation set were saved at {}".format(path_to_store_eval_scores))
+        # Save evaluation results on eval set
+        # if "/" in args.model_name:
+        #     args.model_name = args.model_name.replace('/', '-')
+        path_to_store_eval_results = os.path.join(f'{output_dir}_{str(slurm_job_id)}',
+                                                  f'evaluation.csv')
+        if not os.path.exists(os.path.dirname(path_to_store_eval_results)):
+            os.makedirs(os.path.dirname(path_to_store_eval_results))
+        result_report_macro_df.to_csv(path_to_store_eval_results)
+        logging.info(
+            "The evaluation on the evaluation set is done. The results were saved at {}".format(path_to_store_eval_results))
+        # Save scores
+        eval_df['score'] = scores.tolist()
+        path_to_store_eval_scores = os.path.join(f'{output_dir}_{str(slurm_job_id)}',
+                                                 f'scores.csv')
+        if not os.path.exists(os.path.dirname(path_to_store_eval_scores)):
+            os.makedirs(os.path.dirname(path_to_store_eval_scores))
+        eval_df.to_csv(path_to_store_eval_scores, index=False)
+        logging.info("The scores for the evaluation set were saved at {}".format(path_to_store_eval_scores))
